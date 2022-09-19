8c53519db740c1fa3bda5ad7e0ada02020bc5757
kankanbujian <inutimes@gmail.com>
2022-09-19 17:13:03 +0800

    Upload by PicGo

---
+++ a.js
@@ -0,0 +1,14 @@
+
+// const b = require( './b')
+// import b from './b.js'
+const a = 1
+if (   true ) {
+    import b from './b.js'
+console.log('a.b----',b)
+
+}
+console.log('a----', a)
+export default {a}
+// module.exports = {
+//     a
+// }
